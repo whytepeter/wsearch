5da6a55ed8f67951ea31354846f979b3a5cac170
whytepeter <peteremmanuelwhyte@gmail.com>
2020-04-21 23:19:47 +0100

    done with the sign in component UI

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -27,15 +27,28 @@
         class="w-full px-3 py-2 rounded border-2 border-gray-300 font-medium placeholder-gray-600 text-gray-600 focus:outline-none"
       />
     </div>
+
     <button
       class="btn btn-secondary mb-4 w-full font-medium text-gray-700 hover:text-gray-600 hover:shadow-lg shadow"
     >login</button>
-
+    <span
+      class="Forgot-Password cursor-pointer mb-2 mr-2 text-sm s text-green-200 font-medium hover:underline"
+    >Forgot Passowrd</span>
     <div class="text-center text-secondary">
       Don't have an account
       <span
-        class="transition duration-300 cursor-pointer text-gray-100 hover:underline"
+        class="transition duration-300 font-medium cursor-pointer text-green-200 hover:underline"
       >Sign Up</span>
+    </div>
+
+    <div class="Privacy my-6 text-center text-xs text-secondary">
+      By Signing up, you agree to our
+      <span
+        class="transition duration-300 mb-6 font-medium cursor-pointer text-green-200 hover:underline"
+      >Privary Policy</span> and
+      <span
+        class="transition duration-300 mb-6 font-medium cursor-pointer text-green-200 hover:underline"
+      >Terms of Use.</span>
     </div>
   </div>
 </template>
